e9dbc83e80117356c4ff912b3ed8170292811e52
ASUS <se.zanganeh@yahoo.com>
2020-10-01 21:09:31 +0330

    Body

--- lib/main.dart
+++ lib/main.dart
@@ -19,14 +19,200 @@ class UiExample extends StatelessWidget {
           ),
           centerTitle: true,
         ),
-        body: Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Card(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(10))),
-            elevation: 35,
-            child: Container(
-              height: 120,
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                elevation: 35,
+                child: Container(
+                  height: 120,
+                  child: Column(
+                    children: [
+                      Container(
+                        child: Text("data",
+                            style: TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.bold),
+                            textDirection: TextDirection.ltr),
+                        height: 25,
+                        width: 370,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Column(
+                            children: [
+                              Center(
+                                child: Image.network(
+                                  "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+                                  width: 80,
+                                  height: 65,
+                                ),
+                              ),
+                              Text("ssssssss", style: TextStyle(fontSize: 16)),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Center(
+                                child: Image.network(
+                                  "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+                                  width: 80,
+                                  height: 65,
+                                ),
+                              ),
+                              Text("ssssssss", style: TextStyle(fontSize: 16)),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Center(
+                                child: Image.network(
+                                  "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+                                  width: 80,
+                                  height: 65,
+                                ),
+                              ),
+                              Text("ssssssss", style: TextStyle(fontSize: 16)),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: [
+                    Center(
+                      child: Image.network(
+                        "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+                        width: 80,
+                        height: 65,
+                      ),
+                    ),
+                    Text("ssssssss", style: TextStyle(fontSize: 16)),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Center(
+                      child: Image.network(
+                        "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+                        width: 80,
+                        height: 65,
+                      ),
+                    ),
+                    Text("ssssssss", style: TextStyle(fontSize: 16)),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Center(
+                      child: Image.network(
+                        "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+                        width: 80,
+                        height: 65,
+                      ),
+                    ),
+                    Text("ssssssss", style: TextStyle(fontSize: 16)),
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: [
+                    Center(
+                      child: Image.network(
+                        "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+                        width: 80,
+                        height: 65,
+                      ),
+                    ),
+                    Text("ssssssss", style: TextStyle(fontSize: 16)),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Center(
+                      child: Image.network(
+                        "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+                        width: 80,
+                        height: 65,
+                      ),
+                    ),
+                    Text("ssssssss", style: TextStyle(fontSize: 16)),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Center(
+                      child: Image.network(
+                        "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+                        width: 80,
+                        height: 65,
+                      ),
+                    ),
+                    Text("ssssssss", style: TextStyle(fontSize: 16)),
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: [
+                    Center(
+                      child: Image.network(
+                        "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+                        width: 80,
+                        height: 65,
+                      ),
+                    ),
+                    Text("ssssssss", style: TextStyle(fontSize: 16)),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Center(
+                      child: Image.network(
+                        "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+                        width: 80,
+                        height: 65,
+                      ),
+                    ),
+                    Text("ssssssss", style: TextStyle(fontSize: 16)),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Center(
+                      child: Image.network(
+                        "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+                        width: 80,
+                        height: 65,
+                      ),
+                    ),
+                    Text("ssssssss", style: TextStyle(fontSize: 16)),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 15, left: 15),
               child: Column(
                 children: [
                   Container(
@@ -44,9 +230,9 @@ class UiExample extends StatelessWidget {
                         children: [
                           Center(
                             child: Image.network(
-                              "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+                              "https://lh3.googleusercontent.com/proxy/75KvXD-2I23NBT-Tjz1SLHOzHOr6uJqrlP_7Y6q-evXxaAWx5X9TGxW9GdXcBlPcWWRhXqGwjLFLb0VU79cbIiTO9SeEsmxLxcCN3U_YXksA23HL9sbjd2uxX8LIvBVEeWXmAWtInGDTLkI",
                               width: 80,
-                              height: 55,
+                              height: 65,
                             ),
                           ),
                           Text("ssssssss", style: TextStyle(fontSize: 16)),
@@ -56,9 +242,9 @@ class UiExample extends StatelessWidget {
                         children: [
                           Center(
                             child: Image.network(
-                              "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+                              "https://lh3.googleusercontent.com/proxy/75KvXD-2I23NBT-Tjz1SLHOzHOr6uJqrlP_7Y6q-evXxaAWx5X9TGxW9GdXcBlPcWWRhXqGwjLFLb0VU79cbIiTO9SeEsmxLxcCN3U_YXksA23HL9sbjd2uxX8LIvBVEeWXmAWtInGDTLkI",
                               width: 80,
-                              height: 55,
+                              height: 65,
                             ),
                           ),
                           Text("ssssssss", style: TextStyle(fontSize: 16)),
@@ -68,9 +254,9 @@ class UiExample extends StatelessWidget {
                         children: [
                           Center(
                             child: Image.network(
-                              "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+                              "https://lh3.googleusercontent.com/proxy/75KvXD-2I23NBT-Tjz1SLHOzHOr6uJqrlP_7Y6q-evXxaAWx5X9TGxW9GdXcBlPcWWRhXqGwjLFLb0VU79cbIiTO9SeEsmxLxcCN3U_YXksA23HL9sbjd2uxX8LIvBVEeWXmAWtInGDTLkI",
                               width: 80,
-                              height: 55,
+                              height: 65,
                             ),
                           ),
                           Text("ssssssss", style: TextStyle(fontSize: 16)),
@@ -81,7 +267,7 @@ class UiExample extends StatelessWidget {
                 ],
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
