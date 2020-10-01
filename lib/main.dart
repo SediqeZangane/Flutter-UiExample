361dcbcb16b41362b010724e3c7e6a2f4e7f7451
ASUS <se.zanganeh@yahoo.com>
2020-10-01 22:02:32 +0330

    Rename

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(UiBottom());
+}
+class UiBottom extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false
+    );
+  }
+}
